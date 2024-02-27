916d1e62fa24adbadd48f9c96bf69b734fb8cece
Marin Becker <marin.becker@yahoo.fr>
2024-02-27 17:22:40 +0100

    fixed darkMode (finally !!!), which stores preference in localStorage. Also modified .main-title values for better handling of different device sizes.

--- darkMode2.js
+++ darkMode2.js
@@ -1,27 +1,49 @@
-let modePreference = localStorage.getItem('modePreference');
+let prefMode = localStorage.getItem('prefMode');
+
+document.addEventListener("DOMContentLoaded", function() {
+	setMode();
+});
 
 function setMode() {
-	console.log(modePreference);
-	if (modePreference === 'dark')
-		toggleMode();
+	if (prefMode === null) {
+		prefMode = 'light';
+		localStorage.setItem('prefMode', prefMode);
+	}
+	console.log(prefMode);
+	if (prefMode === 'dark')
+	{
+		switchCSSMode();
+		setImagesMode(prefMode);
+		localStorage.setItem('prefMode', 'dark');
+	}
 }
 
-function toggleMode() {	
-	
+function switchCSSMode(){
 	const root = document.documentElement;
 	const background = getComputedStyle(root).getPropertyValue('--background');
 	const text = getComputedStyle(root).getPropertyValue('--text');
 	root.style.setProperty('--background', text);
 	root.style.setProperty('--text', background);
-	
-	let opposite = modePreference === 'dark' ? 'light' : 'dark';
+}
+
+function setImagesMode(newMode){
+	let oldMode = newMode === 'dark' ? 'light' : 'dark';
+
 	const images = document.querySelectorAll('img');
 	images.forEach((image) => {
 		const src = image.getAttribute('src');
-		const newSrc = src.replace(modePreference, opposite);
+		console.log("replacing ", oldMode, " with ", newMode, " in ", src);
+		const newSrc = src.replace(oldMode, newMode);
 		image.setAttribute('src', newSrc);
 	});
+}
 
-	modePreference = opposite;
-	localStorage.setItem('modePreference', modePreference);
+function switchMode() {	
+	let newMode = prefMode === 'dark' ? 'light' : 'dark';
+	setImagesMode(newMode);
+	switchCSSMode();
+	console.log("ex theme :", prefMode);
+	prefMode = newMode;
+	localStorage.setItem('prefMode', prefMode);
+	console.log("new theme :", prefMode);
 }
